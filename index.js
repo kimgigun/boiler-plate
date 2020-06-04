a905c9a7e8726e6ce585eceb04026a5a47e0e561
kimgigun <kimgigun1985@gmail.com>
2020-06-05 01:04:10 +0900

    oh no

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); //body 부분을 가져오는 기능
 const {User} = require("./models/User");
-const config = require('./config/key');
+const config = require('./config/key'); //설정
 
 
 
@@ -14,22 +14,26 @@ app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect(config.mongoURI,{  //몽고 db 조소 받아옴
+mongoose.connect(config.mongoURI,{  //몽고 db 주소 받아옴
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('Mongo db Conected..'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! 뿅뿅'))
+app.get('/', (req, res) => res.send('Hello World! 뿅뿅 ㅋㅋㅋ 지롤'))
 
 app.post('/register', (req, res) => {
-    const user = new User(req.body)
+
+    const user = new User(req.body);
+    
+    console.log('3')
 
     user.save((err, userInfo) =>{
+        console.log('4')
         if(err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
-        })
-    })
+        });
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name:{
@@ -27,6 +29,31 @@ const userSchema = mongoose.Schema({
         type :Number
     }
 });
+
+userSchema.pre('save', function(next){
+    console.log(`this::${this}`);
+    var user = this;
+
+    //비밀번호 암호화 시킨다
+    if(user.isModified('password')){
+        console.log(`1`);
+        
+        bcrypt.genSalt(saltRounds, function(err, salt){
+         
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                console.log(`2`);
+                if(err) return next(err)
+                user.password = hash
+                console.log('hash::'+user.password);
+                next()
+            });
+        })
+    }
+
+    next()
+})
 
 const User = mongoose.model('User', userSchema)
 
